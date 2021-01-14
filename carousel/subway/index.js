676f76f130d98ec0d39543f94e63f9e7f476d534
dohye1 <tsc03018@gmail.com>
2021-01-14 19:50:27 +0900

    chore : 사용되지 않는 변수 제거 및 불필요한 코드 제거

--- carousel/subway/index.js
+++ carousel/subway/index.js
@@ -1,29 +1,26 @@
 const slideList = document.querySelector('.slide_list');
-const slideContents = document.querySelectorAll('.slide_img');
 const pageDots = document.querySelectorAll('.dot_btn');
 
-const SLIDE_LEN = slideContents.length;
 const SLIDE_WIDTH = 1920;
 const SLIDE_SPEED = 400;
 const DOT_ACTIVE = 'dot_active';
 
-let curDot = 0;
+let curIndex = 0;
 
-pageDots[curDot].classList.add(DOT_ACTIVE);
-let curSlide = slideContents[curDot];
+const handleClick = (selectedIndex) => {
+  pageDots[curIndex].classList.remove(DOT_ACTIVE);
+  pageDots[selectedIndex].classList.add(DOT_ACTIVE);
 
-const handleClick = (dotIndex) => {
-  pageDots[curDot].classList.remove(DOT_ACTIVE);
-  pageDots[dotIndex].classList.add(DOT_ACTIVE);
-  console.log(SLIDE_WIDTH * dotIndex);
   slideList.style.transition = `${SLIDE_SPEED}ms`;
   slideList.style.transform = `translate3d(-${
-    SLIDE_WIDTH * dotIndex
+    SLIDE_WIDTH * selectedIndex
   }px, 0px, 0px)`;
-  curDot = dotIndex;
+
+  curIndex = selectedIndex;
 };
 
 function init() {
+  pageDots[curIndex].classList.add(DOT_ACTIVE);
   for (let i = 0; i < pageDots.length; i++) {
     pageDots[i].addEventListener('click', () => handleClick(i));
   }
